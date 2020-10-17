0f0b76c31ef7a7a44e8ce885380ce5144e5fdb0b
geosoft@inbox.ru <geosoft@inbox.ru>
2020-10-17 19:53:28 +0300

    в левом меню выводятся категории

---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) { }
+
+  // метод вызывается автоматически после инициализации компонента
+  ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
+  }
+
+}
