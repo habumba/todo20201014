a686f9e9bf98efc535785248a044c13cab0d24c8
geosoft@inbox.ru <geosoft@inbox.ru>
2020-10-17 22:54:37 +0300

    добавили вывод задач по категории в консоли

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -19,4 +19,7 @@ export class CategoriesComponent implements OnInit {
     console.log(this.categories);
   }
 
+  showTasksByCategory(category: Category) {
+    this.dataHandler.getTasksByCategory(category);
+  }
 }
